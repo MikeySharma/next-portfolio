ae98845a47090c184654132c84af73e2fbc5d9a2
Mikey Sharma <mikeysharma99@gmail.com>
2024-06-21 22:36:22 +0545

    fixed responsiveness

---
+++ app/lib/mail.ts
@@ -0,0 +1,22 @@
+// // import nodemailer from "nodemailer";
+
+export async function sendMail(email: string, name: string, description: string, subject: string) {
+    // const transporter = nodemailer.createTransport({
+    //     host: "smtp.gmail.com",
+    //     port: 587,
+    //     secure: false, // Use `true` for port 465, `false` for all other ports
+    //     auth: {
+    //         user: "omrajs12345@gmail.com",
+    //         pass: "vwob phul olde qmmy",
+    //     },
+    // });
+    // const info = await transporter.sendMail({
+    //     from: `"${name}" <${email}>`, // sender address
+    //     to: "mikeysharma99@gmail.com", // list of receivers
+    //     subject: subject, // Subject line
+    //     text: description, // plain text body
+    //     // html: "<b>Hello world?</b>", // html body
+    // });
+
+    // console.log("Message sent: %s", info.messageId);
+}
--- app/ui/Contact/ContactForm.tsx
+++ app/ui/Contact/ContactForm.tsx
@@ -1,20 +1,34 @@
+'use client';
+import { sendMail } from "@/app/lib/mail";
+import { useState } from "react"
 
 const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [desc, setDesc] = useState('');
+
+    const handleSubmit = () => {
+        sendMail(email, name,  desc, subject);
+    }
     return (
-        <form action="#" method="POST" className="st-contact-form" id="contact-form">
+        <form action="" className="st-contact-form" id="contact-form">
             <div className="st-form-field">
-                <input type="text" name="name" placeholder="Your Name" id="name" required />
+                <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Your Name" id="name" required />
             </div>
             <div className="st-form-field">
-                <input type="text" name="email" placeholder="Your Email" id="email" required />
+                <input type="text" name="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" required />
             </div>
             <div className="st-form-field">
-                <input type="text" name="subject" placeholder="Your Subject" id="subject" required />
+                <input type="text" name="subject" placeholder="Your Subject" value={subject} onChange={(e) => setSubject(e.target.value)} id="subject" required />
             </div>
             <div className="st-form-field">
-                <textarea name="msg" id="msg" rows={10} cols={30} placeholder="Your Message" required></textarea>
+                <textarea name="msg" id="msg" rows={10} cols={30} value={desc} onChange={(e) => setDesc(e.target.value)} placeholder="Your Message" required></textarea>
             </div>
-            <button type="submit" className="st-btn st-style1 st-color1" id="submit" name="submit">Send Message</button>
+            <button type="submit" onClick={(event) => {
+                event?.preventDefault();
+                handleSubmit();
+            }} className="st-btn st-style1 st-color1" id="submit" name="submit">Send Message</button>
         </form>
     )
 }
